b84abc27109977b09298650b3b4e4e5bd195cd32
Shivanand <shivanandreddy1@gmail.com>
2026-09-21 17:52:15 +0530

    ViewJob updated

--- src/pages/jobs/ViewJob.jsx
+++ src/pages/jobs/ViewJob.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-
+import { useUser } from '../../context/UserContext';
 import axios from 'axios';
 import { 
   Briefcase, 
